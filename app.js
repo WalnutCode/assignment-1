a165a9c06bad6f1a91104008c9c0b63ee7f657d5
Jennifer <jennifer@Jennifers-Air.fios-router.home>
2018-03-07 19:00:11 -0500

    adding js

---
+++ app.js
@@ -0,0 +1,12 @@
+var favColor = "blue";
+var favNumber = 10;
+var Happy = true;
+var Months = ["January", "Febuary", "March", "April"]
+var Planets ={
+    favPlanet:"Saturn"
+}
+console.log(favColor, favNumber, Happy, Months, Planets)
+for(var i=0; i<Months.length; i++){
+    console.log(Months[i])
+}
+console.log(Planets.favPlanet)
